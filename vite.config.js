a6f0a8736338dac32e8ed8909324fd62c47f6797
Mohamed Selem <35905302+mselem0@users.noreply.github.com>
2025-12-31 09:44:12 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
 import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
