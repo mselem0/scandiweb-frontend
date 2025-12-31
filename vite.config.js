b7f8e8bbccd820c167151eb701438bc8263e0ca1
Mohamed Selem <35905302+mselem0@users.noreply.github.com>
2025-12-31 09:42:54 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [autoprefixer()],
